5a0d193db134432f89420122308f47484a18f302
Chris C <chris.c.1221@gmail.com>
2016-03-11 13:51:22 -0500

    maybe update minR

--- R/mineR.R
+++ R/mineR.R
@@ -176,8 +176,6 @@ mineR <- function(doc = character(), terms = character(), local = FALSE, lims = 
 
 	if(syn){ # if the user wants synonyns
 		
-
-		message("Formating synonyms...")
 		# check if user provided syn list
 		if(is.null(syn.list)){
 			syn.list <- make.syn(return = T)
@@ -201,6 +199,49 @@ mineR <- function(doc = character(), terms = character(), local = FALSE, lims = 
 		syn.corp <- tm_map(syn.corp, stemDocument)
 		syn.corp <- tm_map(syn.corp, stripWhitespace)
 
+	} else if(!syn){
+		# leave null
+	}
+
+
+
+
+
+	message("Matching terms...")
+
+	for(name in colnames(TDM.go.df)){
+
+		# going to have to add in another step here
+		# maybe only have to alter this?????
+	  	out %>% filter(get(name, envir=as.environment(out)) == 1) %>% select(matches("PDF_Sentence_*")) -> out.test
+
+		# this shouldnt change 
+	    row <- sum(TDM.go.df[,name] != 0) # n words in term
+	    
+		# will need to alter this to add in the syns !!!! #
+	    sums <- colSums(out.test) # n words from go.df that match
+
+	    for(i in 1:length(lims)){
+	    	if(row == i){
+	    		if(any(sums == lims[[i]])){
+	    			terms <- c(terms, paste(name, sum(sums == lims[[i]], na.rm = TRUE)))
+	    		}
+	    	}
+	    }
+
+	}
+
+
+	message(paste0("Writing output to ", output))
+
+	if(!local){
+		writeLines(as.character(terms), output, sep = "\n")
+	}
+
+	# clean up 
+	# system(paste0("cat .tmp/terms_all*.txt > out.txt"))
+	# system("rm -rf .tmp/")
+	# system(paste0("rm ", doc, ".txt ", basename(doc), ".temp.txt"))
 
 
 
