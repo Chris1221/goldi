a2c4bda7fa1eae385810e7b8bce0a810c0c444ae
Chris C <chris.c.1221@gmail.com>
2016-03-14 14:10:01 -0400

    not sure why but tm was commented

--- R/mineR.R
+++ R/mineR.R
@@ -54,7 +54,7 @@ mineR <- function(doc = character(), terms = character(), local = FALSE, lims = 
 
 	library(Rcpp, quietly = T, verbose = F, warn.conflicts = F)
 	library(dplyr, quietly = T, verbose = F, warn.conflicts = F)
-	#library(tm, quietly = T, verbose = F, warn.conflicts = F)
+	library(tm, quietly = T, verbose = F, warn.conflicts = F)
 	library(SnowballC, quietly = T, verbose = F, warn.conflicts = F)
 
 
