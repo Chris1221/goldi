7f047cdc7ab9122a07d5a02661e63cbeb13c992e
Chris C <chris.c.1221@gmail.com>
2016-08-08 12:57:32 +0100

    clears check now

--- tests/testthat/test-mineR.R
+++ tests/testthat/test-mineR.R
@@ -6,7 +6,11 @@ assign("terms", system.file("extdata", "pdf_chunk_1.txt", package = "mineR"), en
 
 assign("doc", system.file("extdata", "parkinson_mitochondria.pdf", package = "mineR"), envir = .GlobalEnv)
 
-load("../../R/sysdata.rda")
+length = 10
+
+lims <- list(1L, 1L, 3L, 4L, 5L, 6L, 7L, 8L, 9L)
+
+log = "/dev/null"
 
 test_that("testing main function", {
 	expect_equal(suppressMessages(mineR(doc = doc, terms = terms, local = FALSE, length = length, output = "test.txt", lims = lims, log = log, return.as.list = TRUE, pdf_read = "R")), out)
