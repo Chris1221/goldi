cfefdd70e69dac7a6b2950e01ad8b8b9a2a4fa4e
Chris C <chris.c.1221@gmail.com>
2016-08-17 16:26:19 +0100

    skip unit tests on windows

--- tests/testthat/test-mineR.R
+++ tests/testthat/test-mineR.R
@@ -11,18 +11,22 @@ lims <- c(1,2,3,3,4,5,6,6,7,8,9)
 log = "/dev/null"
 ouput = "/dev/null"
 
+os <- .Platform$OS.type
 out <- structure(c("ribosomal_chaperone_activity", "Ribosomal chaperone activity"), .Dim = 1:2)
 
+
 test_that("testing main function", {
-	expect_equal(
-	  mineR(doc = doc,
-	        terms = "empty",
-	        output = output,
-	        lims = lims,
-	        log = log,
-	        object = TRUE,
-	        reader = "local",
-	        term_tdm = TDM.go.df),
-	  out)
+  if(os != "unix") skip("Tests are not currently performed on windows.")
+
+  expect_equal(
+    mineR(doc = doc,
+          terms = "empty",
+          output = output,
+          lims = lims,
+          log = log,
+          object = TRUE,
+          reader = "local",
+          term_tdm = TDM.go.df),
+    out)
 })
 
