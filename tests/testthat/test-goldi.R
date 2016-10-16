eaf1b0b45ce56271114b21dafb3506af991f3ac4
Christopher Cole <chris.c.1221@gmail.com>
2016-10-15 23:22:23 -0400

    Column improvement broke tests, so fix expected output

--- tests/testthat/test-goldi.R
+++ tests/testthat/test-goldi.R
@@ -12,8 +12,9 @@ log = "/dev/null"
 ouput = "/dev/null"
 
 os <- .Platform$OS.type
-out <- structure(c("ribosomal_chaperone_activity", "Ribosomal chaperone activity"), .Dim = 1:2)
+#out <- structure(c("ribosomal_chaperone_activity", "Ribosomal chaperone activity"), .Dim = 1:2)
 
+out <- structure(c("ribosomal_chaperone_activity", "In this sentence we will talk about ribosomal chaperone activity"), .Dim = 1:2, .Dimnames = list(NULL, c("Identified Terms", "Context" )))
 
 test_that("testing main function", {
   if(os != "unix") skip("Tests are not currently performed on windows.")
